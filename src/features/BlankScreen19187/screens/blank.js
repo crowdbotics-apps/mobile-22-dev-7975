897232b42ebeea163d2f24f1b3c7fa51b544033e
Crowdbotics <team@crowdbotics.com>
2020-07-22 05:14:34 +0000

    Update components

--- src/features/BlankScreen19187/screens/blank.js
+++ src/features/BlankScreen19187/screens/blank.js
@@ -22,7 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = { Input_3: "", Input_4: "" }
+  state = { Input_4: "" }
 
   render = () => (
     <View
@@ -98,45 +98,6 @@ export class _Blank extends React.Component {
         Press me!
       </Button>
       <Input
-        placeholder="Number Input Placeholder"
-        editable={true}
-        keyboardType="numeric"
-        textStyle={{ fontSize: 20, color: "#000000" }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none",
-          lineHeight: 1
-        }}
-        value={this.state.Input_3}
-        onChangeText={nextValue => this.setState({ Input_3: nextValue })}
-      />
-      <Input
         placeholder="Sample text input placeholder"
         editable={true}
         textStyle={{ fontSize: 20, color: "#000000" }}
@@ -174,6 +135,40 @@ export class _Blank extends React.Component {
         value={this.state.Input_4}
         onChangeText={nextValue => this.setState({ Input_4: nextValue })}
       />
+      <Text
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none",
+          lineHeight: 1
+        }}
+      >
+        Sample text content
+      </Text>
       <Text
         style={{
           marginLeft: 0,
